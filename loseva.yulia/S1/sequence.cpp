aada862e8c4b1af1072252ad7028e0da23695731
yuklya <yuloseva@mail.ru>
2026-03-13 19:36:06 +0300

    fix sequence

--- loseva.yulia/S1/sequence.cpp
+++ loseva.yulia/S1/sequence.cpp
@@ -1,36 +1,10 @@
-#include <iostream>
 #include "sequence.hpp"
 
-namespace loseva
+namespace loseva {
+
+template <typename T>
+Sequence<T>::~Sequence()
 {
-
-void read_sequences(Storage& data)
-{
-  std::string name;
-
-  while (std::cin >> name)
-  {
-    Seq seq;
-
-    int value;
-
-    while (std::cin.peek() != '\n' && std::cin >> value)
-    {
-      seq.push_back(value);
-    }
-
-    data.push_back({name, seq});
-  }
-}
-
-void print_names(const Storage& data)
-{
-  for (auto it = data.begin(); it != data.end(); ++it)
-  {
-    std::cout << (*it).first << " ";
-  }
-
-  std::cout << "\n";
 }
 
 }
