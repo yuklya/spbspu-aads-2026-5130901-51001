9c6c7ca08f4ae80cd88d98b061f0f199656abb25
yuklya <yuloseva@mail.ru>
2026-03-13 18:42:33 +0300

    fix main.cpp

--- loseva.yulia/S1/main.cpp
+++ loseva.yulia/S1/main.cpp
@@ -1,4 +1,50 @@
 #include <iostream>
-int main()
+#include "sequence.hpp"
+
+namespace loseva
 {
+
+void ReadSequences(Storage& data)
+{
+  std::string name;
+
+  while (std::cin >> name)
+  {
+    Seq seq;
+
+    int value;
+
+    while (std::cin.peek() != '\n' && std::cin >> value)
+    {
+      seq.push_front(value);
+    }
+
+    data.push_front({name, seq});
+  }
+}
+
+void PrintNames(const Storage& data)
+{
+  for (auto it = data.begin(); it != data.end(); ++it)
+  {
+    std::cout << (*it).first << " ";
+  }
+  std::cout << "\n";
+}
+
+void Process(const Storage& data)
+{
+  for (auto it = data.begin(); it != data.end(); ++it)
+  {
+    auto seq = (*it).second;
+
+    for (auto sit = seq.begin(); sit != seq.end(); ++sit)
+    {
+      std::cout << *sit << " ";
+    }
+
+    std::cout << "\n";
+  }
+}
+
 }
