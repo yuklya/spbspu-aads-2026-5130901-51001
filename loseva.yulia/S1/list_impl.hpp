d8e17d859213bc913fcdc70d45de7f3ac973eb75
yuklya <yuloseva@mail.ru>
2026-03-13 12:33:42 +0300

    realize list func

--- loseva.yulia/S1/list_impl.hpp
+++ loseva.yulia/S1/list_impl.hpp
@@ -15,7 +15,35 @@ namespace loseva {
       return new typename List<T>::Node(std::move(value), next, prev);
     }
   }
+  template<typename T>
+  List<T>::List() : head(nullptr), tail(nullptr), size(0) {}
 
+  template<typename T>
+  List<T>::List(size_t count, const T& value) : head(nullptr), tail(nullptr), size(0) {
+    for (size_t i = 0; i < count; ++i) {
+      push_back(value);
+    }
+  }
+  template<typename T>
+  List<T>::List(std::initializer_list<T> init) : head(nullptr), tail(nullptr), size(0) {
+    for (const auto& item : init) {
+      push_back(item);
+    }
+  }
+  template<typename T>
+  List<T>::~List() {
+    clear();
+  }
+  template<typename T>
+  void List<T>::clear() {
+    while (head) {
+      Node* temp = head;
+      head = head->next;
+      delete temp;
+    }
+    tail = nullptr;
+    size = 0;
+  }
 }
 
 #endif
