5fe09a4f29fad9ca387ebb62d8552a17fbbe641b
yuklya <yuloseva@mail.ru>
2026-03-13 12:29:00 +0300

    fix detail

--- loseva.yulia/S1/list_impl.hpp
+++ loseva.yulia/S1/list_impl.hpp
@@ -8,8 +8,14 @@ namespace loseva {
   namespace detail {
     template<typename T>
     typename List<T>::Node* create_node(const T& value, typename List<T>::Node* next = nullptr, typename List<T>::Node* prev = nullptr) {
-    return new typename List<T>::Node(value, next, prev);
+      return new typename List<T>::Node(value, next, prev);
+    }
+    template<typename T>
+    typename List<T>::Node* create_node(T&& value, typename List<T>::Nod> typename List<T>::Nod>
+      return new typename List<T>::Node(std::move(value), next, prev);
+    }
   }
+
 }
 
 #endif
