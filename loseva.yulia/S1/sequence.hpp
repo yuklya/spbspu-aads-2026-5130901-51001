8442e644ddc64f0df795b21770b0ce1c76a14026
yuklya <yuloseva@mail.ru>
2026-03-14 11:39:56 +0300

    edit sequence

--- loseva.yulia/S1/sequence.hpp
+++ loseva.yulia/S1/sequence.hpp
@@ -1,32 +1,21 @@
-#ifndef SEQUENCE_HPP
-#define SEQUENCE_HPP
+#ifndef LOSEVA_SEQUENCE_HPP
+#define LOSEVA_SEQUENCE_HPP
 
 #include <cstddef>
 
-namespace loseva {
-
-template <typename T>
-class Sequence
+namespace loseva
 {
-public:
-  virtual ~Sequence() = default;
+  template<class T>
+  class Sequence
+  {
+  public:
+    virtual ~Sequence() = default;
 
-  virtual void pushBack(const T &value) = 0;
-  virtual void pushFront(const T &value) = 0;
+    virtual void push_front(const T& value) = 0;
+    virtual void pop_front() = 0;
 
-  virtual void popBack() = 0;
-  virtual void popFront() = 0;
-
-  virtual std::size_t size() const = 0;
-  virtual bool empty() const = 0;
-
-  virtual T &front() = 0;
-  virtual const T &front() const = 0;
-
-  virtual T &back() = 0;
-  virtual const T &back() const = 0;
-};
-
+    virtual bool empty() const = 0;
+  };
 }
 
 #endif
