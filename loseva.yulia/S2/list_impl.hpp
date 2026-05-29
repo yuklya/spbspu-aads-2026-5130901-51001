7e9a15523f068ceaed4cbe3d6e739b9395b9655d
yuklya <yuloseva@mail.ru>
2026-05-29 19:52:54 +0300

    fix type in list_impl.hpp

--- loseva.yulia/S2/list_impl.hpp
+++ loseva.yulia/S2/list_impl.hpp
@@ -1,219 +1,253 @@
 #ifndef LOSEVA_LIST_IMPL_HPP
 #define LOSEVA_LIST_IMPL_HPP
 
+#include <cstddef>
+#include <utility>
 #include <stdexcept>
 
 namespace loseva {
 
   template <typename T>
-  class List;
+  class List {
+  private:
+    struct Node {
+      T data;
+      Node* next;
+      Node* prev;
 
-  template <typename T>
-  List<T>::List() : head_(nullptr), tail_(nullptr), size_(0) {
-  }
+      Node(const T& val) : data(val), next(nullptr), prev(nullptr) {}
+    };
 
-  template <typename T>
-  List<T>::~List() {
-    clear();
-  }
+    Node* head_;
+    Node* tail_;
+    size_t size_;
 
-  template <typename T>
-  List<T>::List(const List& other) : List() {
-    for (auto it = other.begin(); it != other.end(); ++it) {
-      push_back(*it);
-    }
-  }
+  public:
+    class iterator {
+      Node* ptr_;
+    public:
+      iterator(Node* p = nullptr) : ptr_(p) {}
 
-  template <typename T>
-  List<T>& List<T>::operator=(const List& other) {
-    if (this != &other) {
+      T& operator*() { return ptr_->data; }
+      T* operator->() { return &ptr_->data; }
+
+      iterator& operator++() { ptr_ = ptr_->next; return *this; }
+      iterator operator++(int) { iterator tmp = *this; ++(*this); return tmp; }
+
+      iterator& operator--() { ptr_ = ptr_->prev; return *this; }
+
+      bool operator==(const iterator& other) const { return ptr_ == other.ptr_; }
+      bool operator!=(const iterator& other) const { return ptr_ != other.ptr_; }
+
+      friend class List;
+    };
+
+    class const_iterator {
+      const Node* ptr_;
+    public:
+      const_iterator(const Node* p = nullptr) : ptr_(p) {}
+
+      const T& operator*() const { return ptr_->data; }
+
+      const_iterator& operator++() { ptr_ = ptr_->next; return *this; }
+      bool operator!=(const const_iterator& other) const {
+        return ptr_ != other.ptr_;
+      }
+
+      friend class List;
+    };
+
+    List() : head_(nullptr), tail_(nullptr), size_(0) {}
+
+    ~List() {
       clear();
+    }
+
+    List(const List& other) : head_(nullptr), tail_(nullptr), size_(0) {
       for (auto it = other.begin(); it != other.end(); ++it) {
         push_back(*it);
       }
     }
-    return *this;
-  }
 
-  template <typename T>
-  List<T>::List(List&& other) noexcept : head_(other.head_), tail_(other.tail_), size_(other.size_) {
-    other.head_ = other.tail_ = nullptr;
-    other.size_ = 0;
-  }
+    List& operator=(const List& other) {
+      if (this != &other) {
+        clear();
+        for (auto it = other.begin(); it != other.end(); ++it) {
+          push_back(*it);
+        }
+      }
+      return *this;
+    }
 
-  template <typename T>
-  List<T>& List<T>::operator=(List&& other) noexcept {
-    if (this != &other) {
-      clear();
-      head_ = other.head_;
-      tail_ = other.tail_;
-      size_ = other.size_;
-
+    List(List&& other) noexcept : head_(other.head_), tail_(other.tail_), size_(other.size_) {
       other.head_ = other.tail_ = nullptr;
       other.size_ = 0;
     }
-    return *this;
-  }
 
-  template <typename T>
-  bool List<T>::empty() const {
-    return size_ == 0;
-  }
+    List& operator=(List&& other) noexcept {
+      if (this != &other) {
+        clear();
+        head_ = other.head_;
+        tail_ = other.tail_;
+        size_ = other.size_;
 
-  template <typename T>
-  size_t List<T>::size() const {
-    return size_;
-  }
-
-  template <typename T>
-  void List<T>::clear() {
-    while (!empty()) {
-      pop_front();
-    }
-  }
-
-  template <typename T>
-  T& List<T>::front() {
-    if (empty()) {
-      throw std::underflow_error("List is empty");
-    }
-    return head_->data;
-  }
-
-  template <typename T>
-  const T& List<T>::front() const {
-    if (empty()) {
-      throw std::underflow_error("List is empty");
-    }
-    return head_->data;
-  }
-
-  template <typename T>
-  T& List<T>::back() {
-    if (empty()) {
-      throw std::underflow_error("List is empty");
-    }
-    return tail_->data;
-  }
-
-  template <typename T>
-  const T& List<T>::back() const {
-    if (empty()) {
-      throw std::underflow_error("List is empty");
-    }
-    return tail_->data;
-  }
-
-  template <typename T>
-  void List<T>::push_back(const T& val) {
-    Node* n = new Node(val);
-    if (tail_) {
-      tail_->next = n;
-      n->prev = tail_;
-      tail_ = n;
-    } else {
-      head_ = tail_ = n;
-    }
-    ++size_;
-  }
-
-  template <typename T>
-  void List<T>::push_front(const T& val) {
-    Node* n = new Node(val);
-    if (head_) {
-      head_->prev = n;
-      n->next = head_;
-      head_ = n;
-    } else {
-      head_ = tail_ = n;
-    }
-    ++size_;
-  }
-
-  template <typename T>
-  void List<T>::pop_back() {
-    if (empty()) {
-      throw std::underflow_error("List is empty");
-    }
-    Node* tmp = tail_;
-    tail_ = tail_->prev;
-
-    if (tail_) {
-      tail_->next = nullptr;
-    } else {
-      head_ = nullptr;
-    }
-    delete tmp;
-    --size_;
-  }
-
-  template <typename T>
-  void List<T>::pop_front() {
-    if (empty()) {
-      throw std::underflow_error("List is empty");
-    }
-    Node* tmp = head_;
-    head_ = head_->next;
-
-    if (head_) {
-      head_->prev = nullptr;
-    } else {
-      tail_ = nullptr;
-    }
-    delete tmp;
-    --size_;
-  }
-
-  template <typename T>
-  typename List<T>::iterator
-  List<T>::insert(iterator pos, const T& val) {
-    if (pos.ptr_ == nullptr) {
-      push_back(val);
-      return iterator(tail_);
+        other.head_ = other.tail_ = nullptr;
+        other.size_ = 0;
+      }
+      return *this;
     }
 
-    Node* cur = pos.ptr_;
-    Node* n = new Node(val);
-
-    n->next = cur;
-    n->prev = cur->prev;
-
-    if (cur->prev) {
-      cur->prev->next = n;
-    } else {
-      head_ = n;
+    bool empty() const {
+      return size_ == 0;
     }
 
-    cur->prev = n;
-
-    ++size_;
-    return iterator(n);
-  }
-
-  template <typename T>
-  typename List<T>::iterator
-  List<T>::erase(iterator pos) {
-    if (pos.ptr_ == nullptr) {
-      return end();
+    size_t size() const {
+      return size_;
     }
-    Node* cur = pos.ptr_;
-    Node* next = cur->next;
 
-    if (cur->prev) {
-      cur->prev->next = cur->next;
-    } else {
-      head_ = cur->next;
+    void clear() {
+      while (!empty()) {
+        pop_front();
+      }
     }
-    if (cur->next) {
-      cur->next->prev = cur->prev;
-    } else {
-      tail_ = cur->prev;
-    }
-    delete cur;
-    --size_;
 
-    return iterator(next);
-  }
+    T& front() {
+      if (empty()) {
+        throw std::underflow_error("List is empty");
+      }
+      return head_->data;
+    }
+
+    const T& front() const {
+      if (empty()) {
+        throw std::underflow_error("List is empty");
+      }
+      return head_->data;
+    }
+
+    T& back() {
+      if (empty()) {
+        throw std::underflow_error("List is empty");
+      }
+      return tail_->data;
+    }
+
+    const T& back() const {
+      if (empty()) {
+        throw std::underflow_error("List is empty");
+      }
+      return tail_->data;
+    }
+
+    void push_back(const T& val) {
+      Node* n = new Node(val);
+      if (tail_) {
+        tail_->next = n;
+        n->prev = tail_;
+        tail_ = n;
+      } else {
+        head_ = tail_ = n;
+      }
+      ++size_;
+    }
+
+    void push_front(const T& val) {
+      Node* n = new Node(val);
+      if (head_) {
+        head_->prev = n;
+        n->next = head_;
+        head_ = n;
+      } else {
+        head_ = tail_ = n;
+      }
+      ++size_;
+    }
+
+    void pop_back() {
+      if (empty()) {
+        throw std::underflow_error("List is empty");
+      }
+      Node* tmp = tail_;
+      tail_ = tail_->prev;
+
+      if (tail_) {
+        tail_->next = nullptr;
+      } else {
+        head_ = nullptr;
+      }
+      delete tmp;
+      --size_;
+    }
+
+    void pop_front() {
+      if (empty()) {
+        throw std::underflow_error("List is empty");
+      }
+      Node* tmp = head_;
+      head_ = head_->next;
+
+      if (head_) {
+        head_->prev = nullptr;
+      } else {
+        tail_ = nullptr;
+      }
+      delete tmp;
+      --size_;
+    }
+
+    iterator begin() { return iterator(head_); }
+    iterator end() { return iterator(nullptr); }
+
+    const_iterator begin() const { return const_iterator(head_); }
+    const_iterator end() const { return const_iterator(nullptr); }
+
+    iterator insert(iterator pos, const T& val) {
+      if (pos.ptr_ == nullptr) {
+        push_back(val);
+        return iterator(tail_);
+      }
+
+      Node* cur = pos.ptr_;
+      Node* n = new Node(val);
+
+      n->next = cur;
+      n->prev = cur->prev;
+
+      if (cur->prev) {
+        cur->prev->next = n;
+      } else {
+        head_ = n;
+      }
+
+      cur->prev = n;
+
+      ++size_;
+      return iterator(n);
+    }
+
+    iterator erase(iterator pos) {
+      if (pos.ptr_ == nullptr) {
+        return end();
+      }
+      Node* cur = pos.ptr_;
+      Node* next = cur->next;
+
+      if (cur->prev) {
+        cur->prev->next = cur->next;
+      } else {
+        head_ = cur->next;
+      }
+      if (cur->next) {
+        cur->next->prev = cur->prev;
+      } else {
+        tail_ = cur->prev;
+      }
+      delete cur;
+      --size_;
+
+      return iterator(next);
+    }
+  };
 
 }
 
