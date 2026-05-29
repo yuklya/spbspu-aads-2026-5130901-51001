ee752a1d3c7c94b4d49b6adb210b19304305404b
yuklya <yuloseva@mail.ru>
2026-05-29 20:12:23 +0300

    fix define

--- loseva.yulia/S2/test-list.cpp
+++ loseva.yulia/S2/test-list.cpp
@@ -1,7 +1,6 @@
-#define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE ListTests
 #include <stdexcept>
-#include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>
 #include "list.hpp"
 
 BOOST_AUTO_TEST_SUITE(ListTests)
