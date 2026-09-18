0e3306db6d4c0acb6246be6fbe9ef0df9896d629
yuklya <yuloseva@mail.ru>
2026-09-18 12:46:43 +0300

    add generation exception in print

--- loseva.yulia/F0/cmd_print.cpp
+++ loseva.yulia/F0/cmd_print.cpp
@@ -6,20 +6,19 @@ void cmdPrint(std::istream & is, std::ostream & os, DatasetsMap & dms)
 {
   std::string name;
   if (!(is >> name) || !dms.count(name)) {
-    os << "<INVALID COMMAND>\n";
+    throw CommandException("<INVALID COMMAND>");
+  }
+  const Dataset & ds = dms[name];
+  if (ds.empty()) {
+    os << "<EMPTY>\n";
   } else {
-    const Dataset & ds = dms[name];
-    if (ds.empty()) {
-      os << "<EMPTY>\n";
-    } else {
-      bool first = true;
-      for (auto it = ds.cbegin(); it != ds.cend(); ++it) {
-        if (!first) os << " ";
-        os << *it;
-        first = false;
-      }
-      os << "\n";
+    bool first = true;
+    for (auto it = ds.cbegin(); it != ds.cend(); ++it) {
+      if (!first) os << " ";
+      os << *it;
+      first = false;
     }
+    os << "\n";
   }
 }
 
