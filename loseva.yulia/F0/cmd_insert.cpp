ba6c19e692197cc0f9709403afeaae9283eb4c42
yuklya <yuloseva@mail.ru>
2026-09-18 12:41:36 +0300

    add generation exception in insert

--- loseva.yulia/F0/cmd_insert.cpp
+++ loseva.yulia/F0/cmd_insert.cpp
@@ -7,12 +7,12 @@ void cmdInsert(std::istream & is, std::ostream & os, DatasetsMap & dms)
   std::string name;
   int val = 0;
   if (!(is >> name >> val) || !dms.count(name)) {
-    os << "<INVALID COMMAND>\n";
-  } else if (dms[name].insert(val)) {
-    os << "В дерево " << name << " добавлен элемент: " << val << "\n";
-  } else {
-    os << "<INVALID COMMAND>\n";
+    throw CommandException("<INVALID COMMAND>");
   }
+  if (!dms[name].insert(val)) {
+    throw CommandException("<INVALID COMMAND>");
+  }
+  os << "В дерево " << name << " добавлен элемент: " << val << "\n";
 }
 
 }
