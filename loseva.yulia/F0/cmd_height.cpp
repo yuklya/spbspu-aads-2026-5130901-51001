014be0d93fe64fb3c929217663ecf60836f6ac9b
yuklya <yuloseva@mail.ru>
2026-09-18 12:39:49 +0300

    add generation exception in height

--- loseva.yulia/F0/cmd_height.cpp
+++ loseva.yulia/F0/cmd_height.cpp
@@ -6,10 +6,9 @@ void cmdHeight(std::istream & is, std::ostream & os, DatasetsMap & dms)
 {
   std::string name;
   if (!(is >> name) || !dms.count(name)) {
-    os << "<INVALID COMMAND>\n";
-  } else {
-    os << "Высота дерева " << name << ": " << dms[name].height() << "\n";
+    throw CommandException("<INVALID COMMAND>");
   }
+  os << "Высота дерева " << name << ": " << dms[name].height() << "\n";
 }
 
 }
