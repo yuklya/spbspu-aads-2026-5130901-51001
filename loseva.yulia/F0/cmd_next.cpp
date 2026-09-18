a925c888a6eda43db4004777ff7a0e8405c03494
yuklya <yuloseva@mail.ru>
2026-09-18 12:45:24 +0300

    add generation exception in next

--- loseva.yulia/F0/cmd_next.cpp
+++ loseva.yulia/F0/cmd_next.cpp
@@ -6,16 +6,14 @@ void cmdNext(std::istream & is, std::ostream & os, IteratorsMap & ims)
 {
   std::string iterName;
   if (!(is >> iterName) || !ims.count(iterName)) {
-    os << "<INVALID COMMAND>\n";
-  } else {
-    auto & it = ims[iterName];
-    if (it.getNode() == nullptr) {
-      os << "<INVALID COMMAND>\n";
-    } else {
-      ++it;
-      os << "Итератор " << iterName << " перешел к следующему элементу\n";
-    }
+    throw CommandException("<INVALID COMMAND>");
   }
+  auto & it = ims[iterName];
+  if (it.getNode() == nullptr) {
+    throw CommandException("<INVALID COMMAND>");
+  }
+  ++it;
+  os << "Итератор " << iterName << " перешел к следующему элементу\n";
 }
 
 }
