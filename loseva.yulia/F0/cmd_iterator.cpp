e9dd23fa13bdc4960487b2ee081273c30d11244d
yuklya <yuloseva@mail.ru>
2026-09-18 12:43:00 +0300

    add generation exception in iterator

--- loseva.yulia/F0/cmd_iterator.cpp
+++ loseva.yulia/F0/cmd_iterator.cpp
@@ -6,15 +6,13 @@ void cmdIterator(std::istream & is, std::ostream & os, IteratorsMap & ims)
 {
   std::string iterName;
   if (!(is >> iterName) || !ims.count(iterName)) {
-    os << "<INVALID COMMAND>\n";
-  } else {
-    auto it = ims[iterName];
-    if (it.getNode() == nullptr) {
-      os << "<INVALID COMMAND>\n";
-    } else {
-      os << "Текущий элемент итератора " << iterName << ": " << *it << "\n";
-    }
+    throw CommandException("<INVALID COMMAND>");
   }
+  auto it = ims[iterName];
+  if (it.getNode() == nullptr) {
+    throw CommandException("<INVALID COMMAND>");
+  }
+  os << "Текущий элемент итератора " << iterName << ": " << *it << "\n";
 }
 
 }
