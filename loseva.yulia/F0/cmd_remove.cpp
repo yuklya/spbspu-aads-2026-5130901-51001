8f49403c51c86852a7a599fc6cdde16051cbc143
yuklya <yuloseva@mail.ru>
2026-09-18 12:47:42 +0300

    add generation exception in remove

--- loseva.yulia/F0/cmd_remove.cpp
+++ loseva.yulia/F0/cmd_remove.cpp
@@ -7,12 +7,12 @@ void cmdRemove(std::istream & is, std::ostream & os, DatasetsMap & dms)
   std::string name;
   int val = 0;
   if (!(is >> name >> val) || !dms.count(name)) {
-    os << "<INVALID COMMAND>\n";
-  } else if (dms[name].remove(val)) {
-    os << "Из дерева " << name << " удалён элемент: " << val << "\n";
-  } else {
-    os << "<INVALID COMMAND>\n";
+    throw CommandException("<INVALID COMMAND>");
   }
+  if (!dms[name].remove(val)) {
+    throw CommandException("<INVALID COMMAND>");
+  }
+  os << "Из дерева " << name << " удалён элемент: " << val << "\n";
 }
 
 }
