82c124458fd70796db2212952eb55fae44c983fc
yuklya <yuloseva@mail.ru>
2026-09-18 12:33:01 +0300

    add generation exception

--- loseva.yulia/F0/cmd_begin.cpp
+++ loseva.yulia/F0/cmd_begin.cpp
@@ -6,12 +6,11 @@ void cmdBegin(std::istream & is, std::ostream & os, DatasetsMap & dms, Iterators
 {
   std::string iterName, treeName;
   if (!(is >> iterName >> treeName) || !dms.count(treeName)) {
-    os << "<INVALID COMMAND>\n";
-  } else {
-    ims[iterName] = dms[treeName].cbegin();
-    os << "Итератор " << iterName << " привязан к дереву " << treeName
-       << " и установлен на его начало\n";
+    throw CommandException("<INVALID COMMAND>");
   }
+  ims[iterName] = dms[treeName].cbegin();
+  os << "Итератор " << iterName << " привязан к дереву " << treeName
+     << " и установлен на его начало\n";
 }
 
 }
