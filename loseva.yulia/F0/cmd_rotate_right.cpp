d9d26106e75030a9f21a0010ac33d4e11bc9ff1e
yuklya <yuloseva@mail.ru>
2026-09-18 12:49:41 +0300

    add generation exception in rotate right

--- loseva.yulia/F0/cmd_rotate_right.cpp
+++ loseva.yulia/F0/cmd_rotate_right.cpp
@@ -7,14 +7,13 @@ void cmdRotateRight(std::istream & is, std::ostream & os, DatasetsMap & dms)
   std::string name;
   int val = 0;
   if (!(is >> name >> val) || !dms.count(name)) {
-    os << "<INVALID COMMAND>\n";
-  } else {
-    try {
-      dms[name].rotateRightManual(val);
-      os << "Выполнен правый поворот для узла " << val << " в дереве " << name << "\n";
-    } catch (...) {
-      os << "<INVALID COMMAND>\n";
-    }
+    throw CommandException("<INVALID COMMAND>");
+  }
+  try {
+    dms[name].rotateRightManual(val);
+    os << "Выполнен правый поворот для узла " << val << " в дереве " << name << "\n";
+  } catch (...) {
+    throw CommandException("<INVALID COMMAND>");
   }
 }
 
