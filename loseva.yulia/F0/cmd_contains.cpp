d65f59f348e39683d73d204c64515c91c09abf69
yuklya <yuloseva@mail.ru>
2026-09-18 12:37:05 +0300

    add generation exception in contains

--- loseva.yulia/F0/cmd_contains.cpp
+++ loseva.yulia/F0/cmd_contains.cpp
@@ -7,10 +7,9 @@ void cmdContains(std::istream & is, std::ostream & os, DatasetsMap & dms)
   std::string name;
   int val = 0;
   if (!(is >> name >> val) || !dms.count(name)) {
-    os << "<INVALID COMMAND>\n";
-  } else {
-    os << (dms[name].has(val) ? "true\n" : "false\n");
+    throw CommandException("<INVALID COMMAND>");
   }
+  os << (dms[name].has(val) ? "true\n" : "false\n");
 }
 
 }
