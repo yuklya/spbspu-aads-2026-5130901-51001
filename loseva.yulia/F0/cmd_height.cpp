a5f6b6cf3b822d7912b4aa80010c618ddfea919e
yuklya <yuloseva@mail.ru>
2026-09-19 16:47:28 +0300

    fix height

--- loseva.yulia/F0/cmd_height.cpp
+++ loseva.yulia/F0/cmd_height.cpp
@@ -5,10 +5,14 @@ namespace loseva {
 void cmdHeight(std::istream & is, std::ostream & os, DatasetsMap & dms)
 {
   std::string name;
-  if (!(is >> name) || !dms.count(name)) {
+  if (!(is >> name)) {
     throw CommandException("<INVALID COMMAND>");
   }
-  os << "Высота дерева " << name << ": " << dms[name].height() << "\n";
+  DatasetPair * pTree = dms.find({name, Dataset{}});
+  if (!pTree) {
+    throw CommandException("<INVALID COMMAND>");
+  }
+  os << "Высота дерева " << name << ": " << pTree->tree.height() << "\n";
 }
 
 }
