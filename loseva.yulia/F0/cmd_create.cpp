cbaa6f6bd9c9a5b4ad54a52b2800041a12b9a1bd
yuklya <yuloseva@mail.ru>
2026-09-19 16:46:24 +0300

    fix create

--- loseva.yulia/F0/cmd_create.cpp
+++ loseva.yulia/F0/cmd_create.cpp
@@ -5,10 +5,10 @@ namespace loseva {
 void cmdCreate(std::istream & is, std::ostream & os, DatasetsMap & dms)
 {
   std::string name;
-  if (!(is >> name) || dms.count(name)) {
+  if (!(is >> name) || dms.has({name, Dataset{}})) {
     throw CommandException("<INVALID COMMAND>");
   }
-  dms[name] = Dataset();
+  dms.insert({name, Dataset{}});
   os << "Создано дерево: " << name << "\n";
 }
 
