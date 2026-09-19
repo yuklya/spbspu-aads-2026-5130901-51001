136a76325d1ac43cc6b2646eee73faa895477645
yuklya <yuloseva@mail.ru>
2026-09-19 16:57:17 +0300

    fix rotate right

--- loseva.yulia/F0/cmd_rotate_right.cpp
+++ loseva.yulia/F0/cmd_rotate_right.cpp
@@ -6,11 +6,15 @@ void cmdRotateRight(std::istream & is, std::ostream & os, DatasetsMap & dms)
 {
   std::string name;
   int val = 0;
-  if (!(is >> name >> val) || !dms.count(name)) {
+  if (!(is >> name >> val)) {
+    throw CommandException("<INVALID COMMAND>");
+  }
+  DatasetPair * pTree = dms.find({name, Dataset{}});
+  if (!pTree) {
     throw CommandException("<INVALID COMMAND>");
   }
   try {
-    dms[name].rotateRightManual(val);
+    pTree->tree.rotateRightManual(val);
     os << "Выполнен правый поворот для узла " << val << " в дереве " << name << "\n";
   } catch (...) {
     throw CommandException("<INVALID COMMAND>");
