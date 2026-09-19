687acc8c3ce34601dae1b079ebbdbfef76dfd3c7
yuklya <yuloseva@mail.ru>
2026-09-19 16:44:54 +0300

    fix contains

--- loseva.yulia/F0/cmd_contains.cpp
+++ loseva.yulia/F0/cmd_contains.cpp
@@ -6,10 +6,14 @@ void cmdContains(std::istream & is, std::ostream & os, DatasetsMap & dms)
 {
   std::string name;
   int val = 0;
-  if (!(is >> name >> val) || !dms.count(name)) {
+  if (!(is >> name >> val)) {
     throw CommandException("<INVALID COMMAND>");
   }
-  os << (dms[name].has(val) ? "true\n" : "false\n");
+  DatasetPair * pTree = dms.find({name, Dataset{}});
+  if (!pTree) {
+    throw CommandException("<INVALID COMMAND>");
+  }
+  os << (pTree->tree.has(val) ? "true\n" : "false\n");
 }
 
 }
