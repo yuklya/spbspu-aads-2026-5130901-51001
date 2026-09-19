d67dadeb0bb1ef5f3bf8e8ee21bfac82c76fc2b4
yuklya <yuloseva@mail.ru>
2026-09-19 16:43:54 +0300

    fix clear

--- loseva.yulia/F0/cmd_begin.cpp
+++ loseva.yulia/F0/cmd_begin.cpp
@@ -5,10 +5,19 @@ namespace loseva {
 void cmdBegin(std::istream & is, std::ostream & os, DatasetsMap & dms, IteratorsMap & ims)
 {
   std::string iterName, treeName;
-  if (!(is >> iterName >> treeName) || !dms.count(treeName)) {
+  if (!(is >> iterName >> treeName)) {
     throw CommandException("<INVALID COMMAND>");
   }
-  ims[iterName] = dms[treeName].cbegin();
+  DatasetPair * pTree = dms.find({treeName, Dataset{}});
+  if (!pTree) {
+    throw CommandException("<INVALID COMMAND>");
+  }
+  IteratorPair * pIter = ims.find({iterName, Dataset::const_iterator{}});
+  if (pIter) {
+    pIter->iter = pTree->tree.cbegin();
+  } else {
+    ims.insert({iterName, pTree->tree.cbegin()});
+  }
   os << "Итератор " << iterName << " привязан к дереву " << treeName
      << " и установлен на его начало\n";
 }
