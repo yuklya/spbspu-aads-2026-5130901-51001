ee7d63ca33e626844637ecef3417ec4b56faeb65
yuklya <yuloseva@mail.ru>
2026-09-19 16:55:18 +0300

    fix rotate left

--- loseva.yulia/F0/cmd_rotate_left.cpp
+++ loseva.yulia/F0/cmd_rotate_left.cpp
@@ -6,11 +6,15 @@ void cmdRotateLeft(std::istream & is, std::ostream & os, DatasetsMap & dms)
 {
   std::string name;
   int val = 0;
-  if (!(is >> name >> val) || !dms.count(name)) {
+  if (!(is >> name >> val)) {
+    throw CommandException("<INVALID COMMAND>");
+  }
+  DatasetPair * pTree = dms.find({name, Dataset{}});
+  if (!pTree) {
     throw CommandException("<INVALID COMMAND>");
   }
   try {
-    dms[name].rotateLeftManual(val);
+    pTree->tree.rotateLeftManual(val);
     os << "Выполнен левый поворот для узла " << val << " в дереве " << name << "\n";
   } catch (...) {
     throw CommandException("<INVALID COMMAND>");
