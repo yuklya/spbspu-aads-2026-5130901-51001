33d81452bbbd3178b4b48e46c3aacce5adfe2d86
yuklya <yuloseva@mail.ru>
2026-09-19 16:49:10 +0300

    fix insert

--- loseva.yulia/F0/cmd_insert.cpp
+++ loseva.yulia/F0/cmd_insert.cpp
@@ -6,10 +6,11 @@ void cmdInsert(std::istream & is, std::ostream & os, DatasetsMap & dms)
 {
   std::string name;
   int val = 0;
-  if (!(is >> name >> val) || !dms.count(name)) {
+  if (!(is >> name >> val)) {
     throw CommandException("<INVALID COMMAND>");
   }
-  if (!dms[name].insert(val)) {
+  DatasetPair * pTree = dms.find({name, Dataset{}});
+  if (!pTree || !pTree->tree.insert(val)) {
     throw CommandException("<INVALID COMMAND>");
   }
   os << "В дерево " << name << " добавлен элемент: " << val << "\n";
