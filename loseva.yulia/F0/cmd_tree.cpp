800cfb3919e92129dbe7d4eb405b0230d10a2042
yuklya <yuloseva@mail.ru>
2026-09-19 17:03:33 +0300

    fix tree

--- loseva.yulia/F0/cmd_tree.cpp
+++ loseva.yulia/F0/cmd_tree.cpp
@@ -5,10 +5,14 @@ namespace loseva {
 void cmdTree(std::istream & is, std::ostream & os, DatasetsMap & dms)
 {
   std::string name;
-  if (!(is >> name) || !dms.count(name)) {
+  if (!(is >> name)) {
     throw CommandException("<INVALID COMMAND>");
   }
-  dms[name].printTreeShape(os);
+  DatasetPair * pTree = dms.find({name, Dataset{}});
+  if (!pTree) {
+    throw CommandException("<INVALID COMMAND>");
+  }
+  pTree->tree.printTreeShape(os);
 }
 
 }
